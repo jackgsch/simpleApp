c701dac2271778df22b67575eb4608af43c3ab38
jackgsch <71718961+jackgsch@users.noreply.github.com>
2020-12-22 10:17:29 -0600

    Add files via upload
    1st commit

---
+++ simpleApp/simpleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  simpleApp
+//
+//  Created by 90305189 on 12/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simpleApp/simpleApp/simpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppApp.swift
+//  simpleApp
+//
+//  Created by 90305189 on 12/22/20.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
